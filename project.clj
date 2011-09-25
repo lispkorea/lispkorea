698843f9e6a9d4e9b4434248aa212ac3100d35e1
longfin <longfinfunnel@gmail.com>
2011-09-25 14:46:37 +0900

    -add dependencies and ring configuration

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [congomongo "0.1.4-SNAPSHOT"]
+                 [enlive "1.0.0-SNAPSHOT"]
                  [compojure "0.6.4"]]
   :dev-dependencies [[swank-clojure "1.3.2"]
                      [org.clojure/clojure "1.2.1" :classifier "sources"]
-                     [lein-ring "0.4.5"]])
+                     [lein-ring "0.4.5"]]
+  :ring {:handler lispkorea.handler/app})
