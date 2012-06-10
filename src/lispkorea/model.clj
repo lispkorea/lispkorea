0dd0cc3ab2beaa81bff2e2c1b40e330854881c67
longfin <longfinfunnel@gmail.com>
2012-06-11 02:33:15 +0900

    - lispkorea.model.defcrud 추가, 적용

--- src/lispkorea/model.clj
+++ src/lispkorea/model.clj
@@ -1,7 +1,6 @@
 (ns lispkorea.model
   (:require [monger.core :as mg]
-            [monger.collection :as mc])
-  (:import [com.mongodb MongoOptions ServerAddress]))
+            [monger.collection :as mc]))
 
 (defn connect [& url]
   (if url
@@ -12,12 +11,20 @@
         (mg/connect!)))))
 
 (def ^{:dynamic true} *conn* (connect))
-(def ^{:dynamic true} *db* (mg/get-db "lispkorea"))
+(def ^{:dynamic true} *db* (mg/get-db *conn* "lispkorea"))
+(when *db*
+  (mg/set-db! *db*))
 
 (defmacro defcrud [name]
   `(do
-     (intern ~*ns* '~'get (fn [~'id]
-                            (mc/find-one-as-map ~name {:_id (ObjectId. ~'id)})))
+     (intern ~*ns*
+             '~'get-by-id
+             (fn [~'id]
+               (mc/find-one-as-map ~name {:_id (ObjectId. ~'id)})))
+     (intern ~*ns*
+             '~'get-by
+             (fn [~'p]
+               (mc/find-one-as-map ~name ~'p)))
      (intern ~*ns* '~'add! (fn [~'p]
                              (mc/insert ~name ~'p)))
      (intern ~*ns* '~'edit! (fn [~'p]
--- src/lispkorea/model/post.clj
+++ src/lispkorea/model/post.clj
@@ -4,20 +4,4 @@
   (:import [org.bson.types ObjectId]))
 
 (defstruct post :author :title :content :created-at)
-(model/defcrud "post")
-
-;; (defn get [id]
-;;   (mc/find-one-as-map "posts" {:_id (ObjectId. id)}))
-
-;; (defn add! [p]
-;;   (mc/insert "posts" p))
-
-;; (defn edit! [p]
-;;   (mc/update "posts" {:_id (:_id p)} p))
-
-;; (defn remove! [arg]
-;;   (let [id (or (:_id arg) (ObjectId. arg))]
-;;     (mc/remove "posts" {:_id id})))
-
-
-  
+(model/defcrud "posts")
