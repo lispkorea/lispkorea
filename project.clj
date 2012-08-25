08a10185d18c8da76e6bd55bfe743495b521fbe7
longfin <longfinfunnel@gmail.com>
2012-08-25 10:14:13 +0900

    의존성 업데이트...

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject lispkorea "1.0.0-SNAPSHOT"
   :description "lisp을 좋아하는 사람들의 그룹(http://groups.google.com/group/lisp-korea)의 웹사이트입니다."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.novemberain/monger "1.0.0-beta5"]
-                 [noir "1.2.2"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.novemberain/monger "1.1.2"]
+                 [noir "1.3.0-beta10"]
                  [enlive "1.0.0"]
                  [org.openid4java/openid4java "0.9.5"]]
   :plugins [[lein-beanstalk "0.2.2"]
-            [swank-clojure "1.3.3"]]
+            [lein-ring "0.7.1"]
+            [lein-swank "1.4.4"]]
   :ring {:handler lispkorea.server/handler}
   :main lispkorea.server)
 
