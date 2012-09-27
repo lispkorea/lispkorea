cb4f9f1444349e41571fd9c53fae5b81f9d5603c
longfin <longfinfunnel@gmail.com>
2012-09-27 20:29:27 +0900

    monger 버전 업그레이드

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject lispkorea "1.0.0-SNAPSHOT"
   :description "lisp을 좋아하는 사람들의 그룹(http://groups.google.com/group/lisp-korea)의 웹사이트입니다."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.novemberain/monger "1.1.2"]
+                 [com.novemberain/monger "1.2.0"]
                  [noir "1.3.0-beta10"]
                  [org.openid4java/openid4java "0.9.5"]]
   :plugins [[lein-beanstalk "0.2.2"]
